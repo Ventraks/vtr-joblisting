8477969a81a71e4276899a1cc1ea85a5e83627b5
ventraks <83806248+ventraks@users.noreply.github.com>
2022-05-04 20:11:09 +0200

    Update server.lua

--- server.lua
+++ server.lua
@@ -2,9 +2,7 @@ ESX = nil
 
 TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 
-RegisterServerEvent('joblisting:server:setJob')
-AddEventHandler('joblisting:server:setJob', function()
-	local xPlayer = ESX.GetPlayerFromId(source)
-
-	xPlayer.setJob(jobs.job, 0)
+RegisterServerEvent('joblisting:server:setJob', function()
+    local xPlayer = ESX.GetPlayerFromId(source)
+    xPlayer.setJob(jobs.job, 0)
 end)
